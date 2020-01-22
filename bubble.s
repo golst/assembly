5e54d4be90bb68c29ac22c7ac80627108855ed86
golst <2636687065@qq.com>
2020-01-22 17:48:57 +0800

    "change formed"

--- bubble.s
+++ bubble.s
@@ -1,8 +1,23 @@
+    .code64
     .file "bubble.s"
+    .section .rodata
+PTEXT:
+    .string "%d\n"
+EXAMPLET:
+    .string "hello world\n"
     .text
     .global main
     .type main, @function 
 main:
-    nop
+    push %rbp
+    mov %rsp, %rbp
+    sub $48, %rsp
+    lea EXAMPLET(%rip), %rdi
+    mov $0, %rax
+    call printf
+    add $4, %rsp  
+    push %rax 
+    call exit
+
 
 
